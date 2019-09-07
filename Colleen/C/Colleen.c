9754a6eb2d332ef8652623671be85dc295936df0
eparisot <eparisot@42.student.fr>
2019-09-07 20:39:47 +0200

    sp to tab

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /*
-    com1
+	com1
 */
 
-void func(char *s) {printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10);}
+void func(char *s) {printf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);}
 
 int main()
 {
-    /*
-        com2
-    */
-    char *s = "#include <stdio.h>%c%c/*%c    com1%c*/%c%cvoid func(char *s) {printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10);}%c%cint main()%c{%c    /*%c        com2%c    */%c    char *s = %c%s%c;%c    func(s);%c    return (0);%c}%c";
-    func(s);
-    return (0);
+	/*
+		com2
+	*/
+	char *s = "#include <stdio.h>%c%c/*%c%ccom1%c*/%c%cvoid func(char *s) {printf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);}%c%cint main()%c{%c%c/*%c%c%ccom2%c%c*/%c%cchar *s = %c%s%c;%c%cfunc(s);%c%creturn (0);%c}%c";
+	func(s);
+	return (0);
 }
