1b64f9ae446451924234112d3491eedeb31768f7
eparisot <eparisot@42.student.fr>
2019-09-09 14:47:16 +0200

    Grace ASM OK

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -3,7 +3,7 @@
 %define RFLAG "w"
 
 section .data
-	msg:	db "%1$chello%2$c", 0
+	msg:	db "%%define MAIN _main%2$c%%define FILE %3$cGrace_kid.s%3$c%2$c%%define RFLAG %3$cw%3$c%2$c%2$csection .data%2$c%1$cmsg:%1$cdb %3$c%4$s%3$c, 0%2$c%1$crflag:%1$cdb RFLAG, 0%2$c%1$cfile:%1$cdb FILE, 0%2$c%2$csection .text%2$c%1$cextern _fopen%2$c%1$cextern _fclose%2$c%1$cextern _fprintf%2$c%1$cglobal MAIN%2$c; com1%2$cMAIN:%2$c%1$cpush rbp%2$c%1$cmov rbp, rsp%2$c%2$c%1$cmov rdi, file%2$c%1$cmov rsi, rflag%2$c%1$ccall _fopen%2$c%2$c%1$ccmp rax, 0%2$c%1$cje end%2$c%2$c%1$cmov rbx, rax%2$c%1$cmov rdi, rax%2$c%1$cmov rsi, msg%2$c%1$cmov rdx, 9%2$c%1$cmov rcx, 10%2$c%1$cmov r8, 34%2$c%1$cmov r9, msg%2$c%1$ccall _fprintf%2$c%2$c%1$ccall _fclose%2$c%1$cjmp end%2$c%2$cend:%2$c%1$cpop rbp%2$c%1$cmov rax, 0%2$c%1$cret%2$c", 0
 	rflag:	db RFLAG, 0
 	file:	db FILE, 0
 
@@ -30,12 +30,11 @@ MAIN:
 	mov rdx, 9
 	mov rcx, 10
 	mov r8, 34
-	mov r9, 59
-	mov r10, msg
+	mov r9, msg
 	call _fprintf
 
 	call _fclose
-	je end
+	jmp end
 
 end:
 	pop rbp
