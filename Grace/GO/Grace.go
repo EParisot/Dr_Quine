d730014c7d63183f17bca0bac44353ded3854546
eparisot <eparisot@42.student.fr>
2019-09-10 21:51:24 +0200

    bonnuses Grace OK

--- Grace/GO/Grace.go
+++ Grace/GO/Grace.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-		"fmt"
-		"os"
+	"fmt"
+	"os"
 )
 
 const (
-	S = "test%[2]c%[4]s"
+	S = "package main%[2]c%[2]cimport (%[2]c%[1]c%[3]cfmt%[3]c%[2]c%[1]c%[3]cos%[3]c%[2]c)%[2]c%[2]cconst (%[2]c%[1]cS = %[3]c%[4]s%[3]c%[2]c%[1]cFNAME = %[3]cGrace_kid.go%[3]c%[2]c%[1]cUSELESS = %[3]c%[3]c%[2]c)%[2]c/*%[2]ccom1%[2]c*/%[2]cfunc main() {%[2]c%[1]cf, err := os.Create(FNAME)%[2]c%[1]cif err != nil {return}%[2]c%[1]cfmt.Fprintf(f, S, 9, 10, 34, S)%[2]c%[1]cf.Close()%[2]c}%[2]c"
 	FNAME = "Grace_kid.go"
-	
+	USELESS = ""
 )
 /*
 com1
