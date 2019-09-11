f150600c4a2ed426a286ee8d2739bbd961edaf03
eparisot <eparisot@student.42.fr>
2019-09-11 21:31:20 +0200

    fixed SULly

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CODE char *s = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define CODE char *s = %3$c%4$s%3$c;%2$c%2$c/*%2$c%1$ccom%2$c*/%2$c%2$cint main() {%2$c%1$cCODE%2$c%1$cint i = %5$d;%2$c%1$cchar f[10];%2$c%1$cchar e[8];%2$c%1$cchar c[50];%2$c%1$cchar o[10];%2$c%1$cFILE *fp;%2$c%1$csprintf(f, %3$cSully_%%d.c%3$c, --i);%2$c%1$csprintf(e, %3$cSully_%%d%3$c, i);%2$c%1$cfp = fopen(f, %3$cw%3$c);%2$c%1$cfprintf(fp, s, 9, 10, 34, s, i);%2$c%1$cfclose(fp);%2$c%1$csprintf(c, %3$cgcc -Wall -Wextra -Werror %%s -o %%s%3$c, f, e);%2$c%1$csystem(c);%2$c%1$csprintf(o, %3$c./%%s%3$c, e);%2$c%1$cif (i) system(o);%2$c%1$creturn (0);%2$c}%2$c";
+#define CODE char *s = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define CODE char *s = %3$c%4$s%3$c;%2$c%2$c/*%2$c%1$ccom%2$c*/%2$c%2$cint main() {%2$c%1$cCODE%2$c%1$cint i = %5$d;%2$c%1$cchar f[100];%2$c%1$cchar e[100];%2$c%1$cchar c[100];%2$c%1$cchar o[100];%2$c%1$cFILE *fp;%2$c%1$csprintf(f, %3$cSully_%%d.c%3$c, --i);%2$c%1$csprintf(e, %3$cSully_%%d%3$c, i);%2$c%1$cfp = fopen(f, %3$cw%3$c);%2$c%1$cfprintf(fp, s, 9, 10, 34, s, i);%2$c%1$cfclose(fp);%2$c%1$csprintf(c, %3$cgcc -Wall -Wextra -Werror %%s -o %%s%3$c, f, e);%2$c%1$csystem(c);%2$c%1$csprintf(o, %3$c./%%s%3$c, e);%2$c%1$cif (i > 0) system(o);%2$c%1$creturn (0);%2$c}%2$c";
 
 /*
 	com
@@ -10,10 +10,10 @@
 int main() {
 	CODE
 	int i = 5;
-	char f[10];
-	char e[8];
-	char c[50];
-	char o[10];
+	char f[100];
+	char e[100];
+	char c[100];
+	char o[100];
 	FILE *fp;
 	sprintf(f, "Sully_%d.c", --i);
 	sprintf(e, "Sully_%d", i);
@@ -23,6 +23,6 @@ int main() {
 	sprintf(c, "gcc -Wall -Wextra -Werror %s -o %s", f, e);
 	system(c);
 	sprintf(o, "./%s", e);
-	if (i) system(o);
+	if (i > 0) system(o);
 	return (0);
 }
