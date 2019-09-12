2dc98596ab3bdb83e77ee1918b8c4ae4395cad51
eparisot <eparisot@42.student.fr>
2019-09-12 15:55:36 +0200

    fixed Sully C GO

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CODE char *s = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define CODE char *s = %3$c%4$s%3$c;%2$c%2$c/*%2$c%1$ccom%2$c*/%2$c%2$cint main() {%2$c%1$cCODE%2$c%1$cint i = %5$d;%2$c%1$cchar f[100];%2$c%1$cchar e[100];%2$c%1$cchar c[100];%2$c%1$cchar o[100];%2$c%1$cFILE *fp;%2$c%1$csprintf(f, %3$cSully_%%d.c%3$c, --i);%2$c%1$csprintf(e, %3$cSully_%%d%3$c, i);%2$c%1$cfp = fopen(f, %3$cw%3$c);%2$c%1$cfprintf(fp, s, 9, 10, 34, s, i);%2$c%1$cfclose(fp);%2$c%1$csprintf(c, %3$cgcc -Wall -Wextra -Werror %%s -o %%s%3$c, f, e);%2$c%1$csystem(c);%2$c%1$csprintf(o, %3$c./%%s%3$c, e);%2$c%1$cif (i > 0) system(o);%2$c%1$creturn (0);%2$c}%2$c";
+#define CODE char *s = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define CODE char *s = %3$c%4$s%3$c;%2$c%2$c/*%2$c%1$ccom%2$c*/%2$c%2$cint main() {%2$c%1$cCODE%2$c%1$cint i = %5$d;%2$c%1$cchar f[100];%2$c%1$cchar e[100];%2$c%1$cchar c[100];%2$c%1$cchar o[100];%2$c%1$cFILE *fp;%2$c%1$csprintf(f, %3$cSully_%%d.c%3$c, i);%2$c%1$csprintf(e, %3$cSully_%%d%3$c, i);%2$c%1$cfp = fopen(f, %3$cw%3$c);%2$c%1$cfprintf(fp, s, 9, 10, 34, s, i-1);%2$c%1$cfclose(fp);%2$c%1$csprintf(c, %3$cgcc -Wall -Wextra -Werror %%s -o %%s%3$c, f, e);%2$c%1$csystem(c);%2$c%1$csprintf(o, %3$c./%%s%3$c, e);%2$c%1$cif (i > 0) system(o);%2$c%1$creturn (0);%2$c}%2$c";
 
 /*
 	com
@@ -15,10 +15,10 @@ int main() {
 	char c[100];
 	char o[100];
 	FILE *fp;
-	sprintf(f, "Sully_%d.c", --i);
+	sprintf(f, "Sully_%d.c", i);
 	sprintf(e, "Sully_%d", i);
 	fp = fopen(f, "w");
-	fprintf(fp, s, 9, 10, 34, s, i);
+	fprintf(fp, s, 9, 10, 34, s, i-1);
 	fclose(fp);
 	sprintf(c, "gcc -Wall -Wextra -Werror %s -o %s", f, e);
 	system(c);
--- Sully/GO/Sully.go
+++ Sully/GO/Sully.go
@@ -7,20 +7,25 @@ import (
 )
 
 const (
-	S = "package main%[2]c%[2]cimport (%[2]c%[1]c%[3]cfmt%[3]c%[2]c%[1]c%[3]cos%[3]c%[2]c%[1]c%[3]cos/exec%[3]c%[2]c)%[2]c%[2]cconst (%[2]c%[1]cS = %[3]c%[5]s%[3]c%[2]c%[1]cFILE = %[3]cSully_%%d.go%[3]c%[2]c%[1]cEXE = %[3]c./Sully_%%d%[3]c%[2]c)%[2]c%[2]cfunc main() {%[2]c%[1]cn := %[4]d%[2]c%[1]cn -= 1%[2]c%[1]cfile := fmt.Sprintf(FILE, n)%[2]c%[1]cf, err := os.Create(file)%[2]c%[1]cif err != nil {return}%[2]c%[1]cfmt.Fprintf(f, S, 9, 10, 34, n, S)%[2]c%[1]cerr = f.Close()%[2]c%[1]cif err != nil {return}%[2]c%[1]cexec.Command(%[3]cgo%[3]c, %[3]cbuild%[3]c, file).Run()%[2]c%[1]cif n != 0 {exec.Command(fmt.Sprintf(EXE, n)).Run()}%[2]c}%[2]c"
+	S    = "package main%[2]c%[2]cimport (%[2]c%[1]c%[3]cfmt%[3]c%[2]c%[1]c%[3]cos%[3]c%[2]c%[1]c%[3]cos/exec%[3]c%[2]c)%[2]c%[2]cconst (%[2]c%[1]cS    = %[3]c%[5]s%[3]c%[2]c%[1]cFILE = %[3]cSully_%%d.go%[3]c%[2]c%[1]cEXE  = %[3]c./Sully_%%d%[3]c%[2]c)%[2]c%[2]cfunc main() {%[2]c%[1]cn := %[4]d%[2]c%[2]c%[1]cfile := fmt.Sprintf(FILE, n)%[2]c%[1]cf, err := os.Create(file)%[2]c%[1]cif err != nil {%[2]c%[1]c%[1]creturn%[2]c%[1]c}%[2]c%[1]cfmt.Fprintf(f, S, 9, 10, 34, n-1, S)%[2]c%[1]cerr = f.Close()%[2]c%[1]cif err != nil {%[2]c%[1]c%[1]creturn%[2]c%[1]c}%[2]c%[1]cexec.Command(%[3]cgo%[3]c, %[3]cbuild%[3]c, file).Run()%[2]c%[1]cif n > 0 {%[2]c%[1]c%[1]cexec.Command(fmt.Sprintf(EXE, n)).Run()%[2]c%[1]c}%[2]c}%[2]c"
 	FILE = "Sully_%d.go"
-	EXE = "./Sully_%d"
+	EXE  = "./Sully_%d"
 )
 
 func main() {
 	n := 5
-	n -= 1
 	file := fmt.Sprintf(FILE, n)
 	f, err := os.Create(file)
-	if err != nil {return}
-	fmt.Fprintf(f, S, 9, 10, 34, n, S)
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(f, S, 9, 10, 34, n-1, S)
 	err = f.Close()
-	if err != nil {return}
+	if err != nil {
+		return
+	}
 	exec.Command("go", "build", file).Run()
-	if n != 0 {exec.Command(fmt.Sprintf(EXE, n)).Run()}
+	if n > 0 {
+		exec.Command(fmt.Sprintf(EXE, n)).Run()
+	}
 }
